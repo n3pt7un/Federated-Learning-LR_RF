48a8c732950b2787d235b62152623b29c871fb25
Glauco Rampone <glaucorampone@gmail.com>
2025-02-28 17:53:41 +0000

    renamed:    main.py -> dataset_processing.py

--- dataset_processing.py
+++ dataset_processing.py
@@ -1,19 +1,10 @@
 from datasets import Dataset, DatasetDict
 import pandas as pd
 
-# Load your dataset (example: CSV)
 df = pd.read_csv('../Federated-Random-Forest/OS%20Scan-dataset_labels.csv')
-
-# Convert to Hugging Face Dataset
 dataset = Dataset.from_pandas(df)
-
-# Perform train-test split (80% train, 20% test)
 split_dataset = dataset.train_test_split(test_size=0.2)
-
-# Access train and test datasets
 train_dataset = split_dataset['train']
 test_dataset = split_dataset['test']
-
-# Optionally, save to disk
 train_dataset.to_csv('train_OSscan.csv')
 test_dataset.to_csv('test_OSscan.csv')
